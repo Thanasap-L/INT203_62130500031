72906e6bae5705d93177469c3e0920145e6e9814
Thanasap-L <thanasap.aloy@mail.kmutt.ac.th>
2021-02-02 00:07:11 +0700

    62130500031_groupwork_1

---
+++ main.js
@@ -0,0 +1,47 @@
+let headhot = [
+    {
+        id: "pic1",
+        img: "./images/pre1.jpeg",
+        info: "เป้ อารักษ์ เล่นใหญ่ในอัลบัม BIG อยากให้ได้ฟังจริงๆนะ"
+    },
+
+    {
+        id: "pic2",
+        img: "./images/pre2.jpeg",
+        info: "13 เพลงจาก ‘เจ มณฑล’ ที่เขียนขึ้นมา ‘ด้วยความเคารพ’ ในอาชีพคนทำดนตรี"
+    },
+
+    {
+        id: "pic3",
+        img: "./images/pre3.jpg",
+        info: "เปิดบ้านคุยสารทุกข์สุขดิบกับ ‘บีมบีม’ จาก FEVER"
+    },
+
+    {
+        id: "pic4",
+        img: "./images/pre4.jpeg",
+        info: "ล่องลอยไปในความฝันกับ Peppermint Town อัลบั้มใหม่จาก My Life As Ali Thomas"
+    },
+
+    {
+        id: "pic5",
+        img: "./images/pre5.jpg",
+        info: "เรื่องราวของความฝัน และปัญหาที่อยากก้าวข้ามกับ ‘ป๊อป’ FEVER"
+    },
+
+    {
+        id: "pic6",
+        img: "./images/pre6.jpg",
+        info: "คุยกับ ‘คณะสุเทพฯ’ เมื่อชีวิตคือการเล่นดนตรี เวทีจึงเป็นสนามแห่งความฝัน"
+    }
+];
+
+let currentPage = 0;
+
+function firstbutton() {
+    let newList = document.createElement("p");
+    newList.innerHTML = 0;
+    inf.insertBefore(newList, null);
+    document.getElementById("img-container").setAttribute("src", newId.img);
+    document.getElementById("button-container").setAttribute("src", )
+}
