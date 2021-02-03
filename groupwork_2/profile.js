6b4c74469f03156bfe06ab60c1417fdb02d1027b
Thanasap-L <thanasap.aloy@mail.kmutt.ac.th>
2021-02-03 12:22:31 +0700

    goupwork_2

--- groupwork_2/profile.js
+++ groupwork_2/profile.js
@@ -4,14 +4,9 @@ const app = {
             image: './images/1.jpg',
             name: 'Thanasap Leelapisuth',
             position: 'Student',
-            article: 'Article',
-            follower: 'Followers',
-            rate:'Ratings',
-            numar: '25',
-            numfol: '103',
-            numrate: '7.0',
-            bt1: 'Follow',
-            bt2: 'Chat'
+            article: '25',
+            follower: '103',
+            rating: '7.0',
         }
     },
 }
